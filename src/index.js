4df065662fc5885616203603ff04ad2603883980
Beatriz Diniz <dinizbe@MacBook-Pro-de-Beatriz.local>
2023-01-29 11:28:01 -0300

    teste prod

--- src/index.js
+++ src/index.js
@@ -1,49 +1,34 @@
-// function getComponent() {
-  
-//     return import('lodash')
-//         .then(({ default: _ }) => {
-//             const element = document.createElement('div');
-  
-//             element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//             return element;
-//         })
-//         .catch((error) => 'An error occurred while loading the component');
-//    }
-  
-// getComponent().then((component) => {
-//     document.body.appendChild(component);
-// });
-
-import { Client } from "@notionhq/client";
+// import { Client } from "@notionhq/client";
 const element = document.getElementById("btn")
 
 element.addEventListener("click", teste);
 
 async function teste() {
-    const notion = new Client({
-        auth: "secret_6OXHw329hzdNQYl1Gb1fjbLgo9KwUNzsrP3ApElp1bA"
-      });
+    console.log("teste");
+    return {};
+    // const notion = new Client({
+    //     auth: "secret_6OXHw329hzdNQYl1Gb1fjbLgo9KwUNzsrP3ApElp1bA"
+    //   });
     
-      const response = await notion.databases.query({
-        database_id: "309633b9f084452c85631116486578ef",
-      });
+    //   const response = await notion.databases.query({
+    //     database_id: "309633b9f084452c85631116486578ef",
+    //   });
     
-      let chartValues = {
-        label: [],
-        data: [],
-      }
+    //   let chartValues = {
+    //     label: [],
+    //     data: [],
+    //   }
     
-      let items = response.results
+    //   let items = response.results
   
-      console.log(items)
+    //   console.log(items)
       
-      items.forEach((element, index) => {
+    //   items.forEach((element, index) => {
     
-        chartValues.label[index] = element.properties.Pilar.title[0].plain_text
-        chartValues.data[index] = element.properties.Nível.number
+    //     chartValues.label[index] = element.properties.Pilar.title[0].plain_text
+    //     chartValues.data[index] = element.properties.Nível.number
         
-      });
+    //   });
     
-      return chartValues;
+    //   return chartValues;
 }
